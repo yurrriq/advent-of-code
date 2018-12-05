7f8204d469732c2814206cde5b23d49ef33bac9d
Eric Bailey <eric@ericb.me>
2018-12-05 03:05:40 -0600

    Day 2: Part Two

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@ let
       }:
       mkDerivation {
         pname = "advent-of-code";
-        version = "0.1.1.0";
+        version = "0.2.0.0";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
--- src/Day02.hs
+++ src/Day02.hs
@@ -10,6 +10,8 @@ import           Control.Arrow       ((***))
 import           Data.ByteString     (ByteString)
 import           Data.Hashable       (Hashable)
 import qualified Data.HashMap.Strict as HM
+import           Data.List           (find, tails)
+import Data.Maybe (catMaybes)
 import           Text.Trifecta       (Parser, Result (..), letter, newline,
                                       parseByteString, sepEndBy, some)
 
@@ -19,6 +21,22 @@ type BoxID = String
 type Checksum = Integer
 
 type Frequency a = HM.HashMap a Integer
+
+
+partOne :: ByteString -> Maybe Checksum
+partOne input =
+    case parseByteString boxIDs mempty input of
+      Failure _errDoc -> Nothing
+      Success ids     -> Just (checksum ids)
+
+
+partTwo :: ByteString -> Maybe String
+partTwo input =
+    case parseByteString boxIDs mempty input of
+      Failure _errDoc -> Nothing
+      Success ids     -> case catMaybes (correctBoxIDs <$> tails ids) of
+                           [(a,b)] -> Just (commonLetters a b)
+                           _       -> Nothing
 
 
 boxID :: Parser BoxID
@@ -55,12 +73,23 @@ checksum = uncurry (*) . foldr go (0, 0)
         in f *** g
 
 
-partOne :: ByteString -> Maybe Checksum
-partOne input =
-    case parseByteString boxIDs mempty input of
-         Failure _errDoc -> Nothing
-         Success ids     -> Just (checksum ids)
+hammingDistance :: Eq a => [a] -> [a] -> Maybe Integer
+hammingDistance (x:xs) (y:ys) | x /= y    = (+1) <$> recur
+                              | otherwise = recur
+  where recur = hammingDistance xs ys
+hammingDistance [] []                     = Just 0
+hammingDistance _ _                       = Nothing
 
 
-partTwo :: ByteString -> Maybe ()
-partTwo = undefined
+correctBoxIDs :: [BoxID] -> Maybe (BoxID, BoxID)
+correctBoxIDs (x:xs@(_:_)) = do y <- find ((== Just 1) . hammingDistance x) xs
+                                pure (x,y)
+correctBoxIDs [_]          = Nothing
+correctBoxIDs []           = Nothing
+
+
+commonLetters :: String -> String -> String
+commonLetters (x:xs) (y:ys) | x == y    = x:recur
+                            | otherwise = recur
+  where recur = commonLetters xs ys
+commonLetters _ _ = "" -- NOTE: This is bad.
