68f0e6e98e6798040094487942218490f5c70b73
Eric Bailey <eric@ericb.me>
2021-07-24 04:53:28 -0500

    [2015.07] Add Haskell solution

---
+++ src/AdventOfCode/Year2015/Day07.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+
+module AdventOfCode.Year2015.Day07 where
+
+import AdventOfCode.Input (parseInput)
+import AdventOfCode.TH (inputFilePath)
+import Control.Applicative (liftA2, (<|>))
+import Control.Monad.Reader
+import Data.Bits (Bits (..), complement, shiftL, shiftR, (.&.), (.|.))
+import Data.Function (on)
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+import Data.Word (Word16)
+import Text.Parser.Token.Highlight
+import Text.Trifecta
+import Prelude hiding (and, not, or)
+
+type Circuit = Map String Gate
+
+instance {-# OVERLAPPING #-} Show Circuit where
+  show = unlines . map show . Map.toList
+
+circuit :: Parser Circuit
+circuit = Map.fromList <$> (wire `sepEndBy` newline)
+
+evalCircuit :: Circuit -> Map String Word16
+evalCircuit circ = results
+  where
+    results = Map.map (flip runReader results . evalGate) circ
+
+evalGate :: Gate -> Reader (Map String Word16) Word16
+evalGate (PROVIDE v) = evalValue v
+evalGate (x `AND` y) = evalBinop (.&.) x y
+evalGate (x `LSHIFT` y) = evalBinop shiftL' x y
+evalGate (NOT v) = complement <$> evalValue v
+evalGate (x `OR` y) = evalBinop (.|.) x y
+evalGate (x `RSHIFT` y) = evalBinop shiftR' x y
+
+evalBinop :: (Word16 -> Word16 -> Word16) -> Value -> Value -> Reader (Map String Word16) Word16
+evalBinop f = liftA2 f `on` evalValue
+
+evalValue :: Value -> Reader (Map String Word16) Word16
+evalValue (Name c) = asks (! c)
+evalValue (Signal x) = pure x
+
+data Gate
+  = PROVIDE Value
+  | AND Value Value
+  | LSHIFT Value Value
+  | NOT Value
+  | OR Value Value
+  | RSHIFT Value Value
+  deriving (Eq)
+
+instance Show Gate where
+  show (PROVIDE x) = show x
+  show (x `AND` y) = show x <> " AND " <> show y
+  show (x `LSHIFT` y) = show x <> " LSHIFT " <> show y
+  show (NOT x) = "NOT " <> show x
+  show (x `OR` y) = show x <> " OR " <> show y
+  show (x `RSHIFT` y) = show x <> " RSHIFT " <> show y
+
+gate :: Parser Gate
+gate = try and <|> try lshift <|> try not <|> try or <|> try rshift <|> try provide
+  where
+    and = binop AND "AND"
+    lshift = binop LSHIFT "LSHIFT"
+    not = NOT <$> (symbol "NOT" *> value)
+    or = binop OR "OR"
+    rshift = binop RSHIFT "RSHIFT"
+    provide = PROVIDE <$> value
+    binop f op = f <$> (value <* symbol op) <*> value
+
+shiftL' :: (Bits a, Integral a) => a -> a -> a
+shiftL' x y = shiftL x (fromIntegral y)
+
+shiftR' :: (Bits a, Integral a) => a -> a -> a
+shiftR' x y = shiftR x (fromIntegral y)
+
+type Wire = (String, Gate)
+
+instance {-# OVERLAPPING #-} Show Wire where
+  show (k, v) = show v <> " -> " <> k
+
+wire :: Parser Wire
+wire = flip (,) <$> (gate <* symbol "->") <*> some lower
+
+data Value
+  = Name String
+  | Signal Word16
+  deriving (Eq)
+
+value :: Parser Value
+value = name <|> signal
+  where
+    name = Name <$> token (highlight Identifier (some lower))
+    signal = Signal . fromIntegral <$> natural
+
+instance Show Value where
+  show (Name c) = c
+  show (Signal x) = show x
+
+-- main :: IO ()
+-- main = putStrLn "Not yet implemented"
+
+main :: IO ()
+main =
+  do
+    input <- parseInput circuit $(inputFilePath)
+    putStr "Part One: "
+    let a = partOne input
+    print a
+    putStr "Part Two: "
+    print $ partTwo (Map.insert "b" (PROVIDE (Signal a)) input)
+
+partOne :: Circuit -> Word16
+partOne = (! "a") . evalCircuit
+
+partTwo :: Circuit -> Word16
+partTwo = (! "a") . evalCircuit
---
+++ test/Test/AdventOfCode/Year2015/Day07.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Test.AdventOfCode.Year2015.Day07 where
+
+import AdventOfCode.Year2015.Day07 (circuit, evalCircuit)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+import Test.Tasty
+import Test.Tasty.HUnit
+import Text.Trifecta (Result(..), parseString)
+
+test_day07_examples :: TestTree
+test_day07_examples =
+  testGroup
+    "Examples"
+    [ testCase "the example evaluates correctly" $
+        do
+          let input =
+                fromJust . \case { Success c -> Just c; Failure _ -> Nothing } . parseString circuit mempty . unlines $
+                  [ "123 -> x",
+                    "456 -> y",
+                    "x AND y -> d",
+                    "x OR y -> e",
+                    "x LSHIFT 2 -> f",
+                    "y RSHIFT 2 -> g",
+                    "NOT x -> h",
+                    "NOT y -> i"
+                  ]
+          let expected = Map.fromList [
+                ("d", 72),
+                ("e", 507),
+                ("f", 492),
+                ("g", 114),
+                ("h", 65412),
+                ("i", 65079),
+                ("x", 123),
+                ("y", 456)
+                ]
+          evalCircuit input @?= expected
+    ]
