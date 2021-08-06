fb79107eee19db029930931cc76e629315abf878
Eric Bailey <eric@ericb.me>
2021-08-06 02:33:21 -0500

    [2015.13] Add inefficient Haskell solution

---
+++ src/AdventOfCode/Year2015/Day13.hs
@@ -0,0 +1,65 @@
+module AdventOfCode.Year2015.Day13 where
+
+import AdventOfCode.Input
+import AdventOfCode.TH
+import Control.Applicative ((<|>))
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+import Data.List (permutations)
+import Data.Tuple (swap)
+import Text.Parser.Token.Highlight
+import Text.Trifecta
+
+type PotentialHappiness = HashMap String (HashMap String Integer)
+
+main :: IO ()
+main = $(defaultMain)
+
+getInput :: IO PotentialHappiness
+getInput =
+  HM.fromListWith HM.union
+    <$> parseInput (potentialHappiness `sepEndBy` newline) $(inputFilePath)
+
+partOne :: PotentialHappiness -> Integer
+partOne = maxHappiness
+
+partTwo :: PotentialHappiness -> Integer
+partTwo input =
+  maxHappiness . HM.union (HM.map (HM.insert "Me" 0) input) $
+    HM.singleton "Me" . HM.fromList $
+      zip (invitees input) (repeat 0)
+
+maxHappiness :: PotentialHappiness -> Integer
+maxHappiness input =
+  maximum
+    [ sum [(input HM.! person) HM.! neighbor | (person, neighbor) <- xs]
+      | xs <- map pairs (permutations (invitees input))
+    ]
+
+potentialHappiness :: Parser (String, HashMap String Integer)
+potentialHappiness =
+  do
+    person <- name
+    n <- wouldGain <|> (negate <$> wouldLose)
+    neighbor <- bySittingNextTo name
+    pure (person, HM.singleton neighbor n)
+
+name :: Parser String
+name = highlight Identifier (token (some letter))
+
+wouldGain :: Parser Integer
+wouldGain = highlight Operator (symbol "would gain") *> natural
+
+wouldLose :: Parser Integer
+wouldLose = highlight Operator (symbol "would lose") *> natural
+
+bySittingNextTo :: Parser a -> Parser a
+bySittingNextTo = between (string "happiness units by sitting next to ") (char '.')
+
+pairs :: [a] -> [(a, a)]
+pairs xs = concatMap ((:) <$> swap <*> return) ps
+  where
+    ps = (head xs, last xs) : zip xs (tail xs)
+
+invitees :: PotentialHappiness -> [String]
+invitees = HM.keys
