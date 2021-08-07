811e354bed9c1340ef8816306dee2c2301757730
Eric Bailey <eric@ericb.me>
2021-08-07 17:18:06 -0500

    [2015.17] Add simple Haskell solution

---
+++ src/AdventOfCode/Year2015/Day17.hs
@@ -0,0 +1,32 @@
+module AdventOfCode.Year2015.Day17
+  ( main,
+    partOne,
+    partTwo,
+    getInput,
+  )
+where
+
+import AdventOfCode.Input (parseInput)
+import AdventOfCode.TH (defaultMain, inputFilePath)
+import Data.List (group, sort, subsequences)
+import Text.Trifecta (integer, some)
+
+main :: IO ()
+main = $(defaultMain)
+
+partOne :: [Int] -> Int
+partOne = length . solutions
+
+partTwo :: [Int] -> Int
+partTwo =
+  minimum . map length
+    . group
+    . sort
+    . map length
+    . solutions
+
+getInput :: IO [Int]
+getInput = parseInput (some (fromInteger <$> integer)) $(inputFilePath)
+
+solutions :: [Int] -> [[Int]]
+solutions = filter ((150 ==) . sum) . subsequences
