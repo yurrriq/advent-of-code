cbefccf328218350b0025ef1e567279563639b07
Eric Bailey <eric@ericb.me>
2021-09-11 03:35:27 -0500

    AdventOfCode.Input: add parseString

--- src/AdventOfCode/Input.hs
+++ src/AdventOfCode/Input.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
+
 module AdventOfCode.Input where
 
+import Control.Arrow ((>>>))
 import Control.Monad.IO.Class (liftIO)
 import Paths_advent_of_code (getDataFileName)
 import Text.Trifecta (Parser, Result (..), parseFromFileEx)
+import qualified Text.Trifecta as Trifecta
 
 parseInput :: Parser a -> FilePath -> IO a
 parseInput parser fname =
@@ -12,3 +16,9 @@ parseInput parser fname =
     case parsed of
       Success result -> pure result
       Failure reason -> error (show reason)
+
+parseString :: Parser a -> String -> IO a
+parseString parser =
+  Trifecta.parseString parser mempty >>> \case
+    Success result -> pure result
+    Failure reason -> error (show reason)
