34741c67053c33f1495908beaabfeaca2d6ca369
Eric Bailey <eric@ericb.me>
2021-12-01 18:40:20 -0600

    [2021.01] Add simple Haskell solution to Part One

---
+++ src/AdventOfCode/Year2021/Day01.hs
@@ -0,0 +1,21 @@
+module AdventOfCode.Year2021.Day01 where
+
+import AdventOfCode.Input (parseInput)
+import AdventOfCode.TH (defaultMain, inputFilePath)
+import AdventOfCode.Util (count)
+import Text.Trifecta (many, natural)
+
+main :: IO ()
+main = $(defaultMain)
+
+getInput :: IO [Integer]
+getInput = parseInput (many natural) $(inputFilePath)
+
+example :: [Integer]
+example = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+partOne :: [Integer] -> Int
+partOne xs = count (== LT) $ zipWith compare xs (tail xs)
+
+partTwo :: [Integer] -> Int
+partTwo = undefined
