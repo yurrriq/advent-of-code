ab6273f1640a78f9b80c87d6aa6274a1942160d2
Eric Bailey <eric@ericb.me>
2021-12-01 18:50:53 -0600

    [2021.01] Add simple Haskell solution to Part Two

--- src/AdventOfCode/Year2021/Day01.hs
+++ src/AdventOfCode/Year2021/Day01.hs
@@ -3,6 +3,7 @@ module AdventOfCode.Year2021.Day01 where
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
 import AdventOfCode.Util (count)
+import Data.List (tails)
 import Text.Trifecta (many, natural)
 
 main :: IO ()
@@ -18,4 +19,9 @@ partOne :: [Integer] -> Int
 partOne xs = count (== LT) $ zipWith compare xs (tail xs)
 
 partTwo :: [Integer] -> Int
-partTwo = undefined
+partTwo =
+  partOne
+    . map sum
+    . takeWhile ((>= 3) . length)
+    . map (take 3)
+    . tails
