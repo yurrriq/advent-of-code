15c539ee9d33b80e0dccd201c8c8cf1357fc22ac
Eric Bailey <eric@ericb.me>
2021-12-03 17:14:26 -0600

    [2021.03] Add messy Haskell solution to Part Two

--- src/AdventOfCode/Year2021/Day03.hs
+++ src/AdventOfCode/Year2021/Day03.hs
@@ -3,6 +3,10 @@ module AdventOfCode.Year2021.Day03 where
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
 import Control.Applicative ((<|>))
+import Control.Arrow ((&&&))
+import Control.Category ((>>>))
+import Data.Bool (bool)
+import Data.Char (digitToInt)
 import Data.Foldable (foldl')
 import Data.Functor (($>))
 import Data.List (foldl1')
@@ -50,7 +54,28 @@ partOne diagnostics = gammaRate * epsilonRate
         map (map fromEnum) diagnostics
 
 partTwo :: [[Bool]] -> Int
-partTwo = undefined
+partTwo = (generatorRating 0 &&& scrubberRating 0) >>> uncurry (*)
+  where
+    generatorRating = keepBit (>=)
+    scrubberRating = keepBit (<)
+
+    keepBit cmp n diagnostics =
+      case filter p diagnostics of
+        [rating] -> binToDec rating
+        diagnostics' -> keepBit cmp (succ n) diagnostics'
+      where
+        p = bool not id (sums !! n `cmp` pivot) . (!! n)
+        pivot = mkPivot diagnostics
+        sums = mkSums diagnostics
 
 binToDec :: [Bool] -> Int
 binToDec = foldl' (\z b -> fromEnum b + 2 * z) 0
+
+mkPivot :: [a] -> Double
+mkPivot = (/ 2) . fromIntegral . length
+
+mkSums :: [[Bool]] -> [Double]
+mkSums =
+  map fromIntegral
+    . foldl1' (zipWith (+))
+    . map (map fromEnum)
