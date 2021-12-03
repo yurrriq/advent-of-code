588c65d99f542d59dbdfc519cc4b41bceac29c83
Eric Bailey <eric@ericb.me>
2021-12-02 18:04:02 -0600

    [2021.01] Make Haskell solution more clever

--- src/AdventOfCode/Year2021/Day01.hs
+++ src/AdventOfCode/Year2021/Day01.hs
@@ -3,7 +3,7 @@ module AdventOfCode.Year2021.Day01 where
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
 import AdventOfCode.Util (count)
-import Data.List (tails)
+import Control.Arrow ((&&&))
 import Text.Trifecta (natural, some)
 
 main :: IO ()
@@ -16,12 +16,13 @@ example :: [Integer]
 example = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
 partOne :: [Integer] -> Int
-partOne xs = count (== LT) $ zipWith compare xs (tail xs)
+partOne = countPairwiseIncreases 1
 
 partTwo :: [Integer] -> Int
-partTwo =
-  partOne
-    . map sum
-    . takeWhile ((>= 3) . length)
-    . map (take 3)
-    . tails
+partTwo = countPairwiseIncreases 3
+
+countPairwiseIncreases :: Ord a => Int -> [a] -> Int
+countPairwiseIncreases n =
+  count (== LT)
+    . uncurry (zipWith compare)
+    . (id &&& drop n)
