5ee4f8d74b35ffd0b65f419b7b52a8119768f392
Eric Bailey <eric@ericb.me>
2021-12-03 18:39:50 -0600

    [2021.02] Use the semi-direct product of a monoid
    https://github.com/mstksg/advent-of-code-2021/blob/master/reflections-out/day02.md

--- src/AdventOfCode/Year2021/Day02.hs
+++ src/AdventOfCode/Year2021/Day02.hs
@@ -1,62 +1,65 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module AdventOfCode.Year2021.Day02 where
 
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
 import Control.Applicative ((<|>))
-import Control.Lens (view, (+~), (-~))
-import Data.Foldable (foldl')
 import Data.Functor (($>))
-import Linear (V2 (..), V3 (..), _x, _xy, _y, _z)
-import Text.Trifecta (Parser, many, natural, symbol)
+import Data.Monoid.Action (Action (..))
+import Data.Monoid.SemiDirectProduct.Strict (Semi, embed, inject, untag)
+import Data.Semigroup (Sum (..), getSum)
+import Linear (V2 (..))
+import Text.Trifecta (Parser, natural, some, symbol)
 
-data Command a
-  = Cmd Direction a
-  deriving (Eq, Show)
+type Direction = Sum (V2 Int)
 
-data Direction
-  = Forward
-  | Down
-  | Up
-  deriving (Eq, Show)
+newtype Aim = Aim Int
+  deriving
+    (Semigroup, Monoid)
+    via (Sum Int)
+
+instance Action Aim Direction where
+  act (Aim a) (Sum (V2 x y)) = Sum (V2 x (y + a * x))
 
 main :: IO ()
 main = $(defaultMain)
 
-getInput :: IO [Command Integer]
-getInput = parseInput (many command) $(inputFilePath)
+getInput :: IO [Direction]
+getInput = parseInput (some direction) $(inputFilePath)
 
-example :: [Command Integer]
+example :: [Direction]
 example =
-  [ Cmd Forward 5,
-    Cmd Down 5,
-    Cmd Forward 8,
-    Cmd Up 3,
-    Cmd Down 8,
-    Cmd Forward 2
+  [ forward 5,
+    down 5,
+    forward 8,
+    up 3,
+    down 8,
+    forward 2
   ]
 
-partOne :: [Command Integer] -> Integer
-partOne = product . foldl' (flip runCommand) (pure 0)
-  where
-    runCommand :: Command Integer -> V2 Integer -> V2 Integer
-    runCommand (Cmd Forward n) = _x +~ n
-    runCommand (Cmd Down n) = _y +~ n
-    runCommand (Cmd Up n) = _y -~ n
+partOne :: [Direction] -> Int
+partOne = product . getSum . mconcat
 
-partTwo :: [Command Integer] -> Integer
-partTwo = product . view _xy . foldl' runCommand (pure 0)
+partTwo :: [Direction] -> Int
+partTwo = product . getSum . untag . mconcat . map transform
   where
-    runCommand :: V3 Integer -> Command Integer -> V3 Integer
-    runCommand pos (Cmd Forward n) =
-      (_x +~ n) . (_y +~ ((* n) . view _z $ pos)) $ pos
-    runCommand pos (Cmd Down n) = _z +~ n $ pos
-    runCommand pos (Cmd Up n) = _z -~ n $ pos
-
-command :: Parser (Command Integer)
-command = Cmd <$> direction <*> natural
+    transform :: Direction -> Semi Direction Aim
+    transform dir@(Sum (V2 _ 0)) = inject dir
+    transform (Sum (V2 0 y)) = embed (Aim y)
+    transform _ = error "Invalid direction"
 
 direction :: Parser Direction
-direction =
-  symbol "forward" $> Forward
-    <|> symbol "down" $> Down
-    <|> symbol "up" $> Up
+direction = dir <*> (fromInteger <$> natural)
+  where
+    dir =
+      symbol "forward" $> forward
+        <|> symbol "down" $> down
+        <|> symbol "up" $> up
+
+forward, down, up :: Int -> Direction
+forward = pure . flip V2 0
+down = pure . V2 0
+up = down . negate
