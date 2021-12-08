38ba7740bab60def94d4f61716704f59a337235d
Eric Bailey <eric@ericb.me>
2021-12-08 01:43:07 -0600

    [2021.06] Add matrix-based Haskell solution to Part Two

--- src/AdventOfCode/Year2021/Day06.hs
+++ src/AdventOfCode/Year2021/Day06.hs
@@ -2,7 +2,14 @@ module AdventOfCode.Year2021.Day06 where
 
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
+import Control.Monad (forM_)
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Linear (Additive, (!*), (!*!))
 import Text.Trifecta (commaSep, natural)
+
+-- TODO: Data.Finite
+-- TODO: Data.Vector.Sized
 
 main :: IO ()
 main = $(defaultMain)
@@ -17,10 +24,30 @@ partOne :: [Int] -> Int
 partOne = simulate 80
 
 partTwo :: [Int] -> Int
-partTwo = undefined
+partTwo = simulate 256
 
 simulate :: Int -> [Int] -> Int
-simulate n = length . (!! n) . iterate (concatMap step)
+simulate n xs = sum $ (matrix !^! n) !* v
   where
-    step 0 = [6, 8]
-    step t = [t - 1]
+    v = V.modify (forM_ xs . flip MV.modify succ) (V.replicate 9 0)
+
+matrix :: V.Vector (V.Vector Int)
+matrix =
+  V.fromListN 9 . map (V.fromListN 9) $
+    [ [0, 1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+infixl 8 !^!
+
+(!^!) :: (Foldable m, Additive m, Num a) => m (m a) -> Int -> m (m a)
+a !^! n = iterate (!*! a) a !! pred n
+
+-- TODO: stimes with Semigroup
