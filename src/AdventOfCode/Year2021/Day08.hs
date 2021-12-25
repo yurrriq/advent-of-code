6ec5ef70715fcec4d59116dae2f88fc8b3cefa5f
Eric Bailey <eric@ericb.me>
2021-12-24 19:13:03 -0600

    [2021.08] Lazily generate the problem space

--- src/AdventOfCode/Year2021/Day08.hs
+++ src/AdventOfCode/Year2021/Day08.hs
@@ -2,90 +2,63 @@ module AdventOfCode.Year2021.Day08 where
 
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
-import AdventOfCode.Util ((<||>))
 import Data.FastDigits (undigits)
-import Data.Function (on)
-import Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as IM
-import Data.List (find, sort, (\\))
-import Data.List.Ordered (isect)
-import qualified Data.Map.Strict as M
-import Data.Tuple (swap)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
+import Data.List (permutations)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Set (Set)
+import qualified Data.Set as S
 import Text.Trifecta (count, oneOf, some, space, symbol, try)
 
 main :: IO ()
 main = $(defaultMain)
 
-partOne :: [([String], [String])] -> Int
+partOne :: [(Set (Set Char), [Set Char])] -> Int
 partOne = sum . map go
   where
     go = length . filter (`elem` [2, 3, 4, 7]) . map length . snd
 
-partTwo :: [([String], [String])] -> Int
+partTwo :: [(Set (Set Char), [Set Char])] -> Int
 partTwo = sum . map go
   where
     go (patterns, outputs) =
-      fromInteger . undigits (10 :: Int) . reverse $
-        map (digits M.!) outputs
-      where
-        digits =
-          M.fromList . map swap . IM.toList $
-            foldl
-              (flip ($ patterns))
-              (findEasyNumbers patterns)
-              [ deduceNumber 3 1 2,
-                deduceNumber 2 4 2,
-                deduceNumber 5 2 3,
-                deduceNumber 9 3 5,
-                deduceNumber 0 1 2,
-                deduceNumber 6 0 5
-              ]
+      let displays = problemSpace M.! patterns
+       in fromInteger . undigits (10 :: Int) . reverse $
+            map (displays M.!) outputs
 
-getInput :: IO [([String], [String])]
+getInput :: IO [(Set (Set Char), [Set Char])]
 getInput = parseInput (some display) $(inputFilePath)
   where
     display =
       do
-        connections <- count 10 segment
+        connections <- S.fromList <$> count 10 segment
         _ <- symbol "|"
         output <- count 4 segment
         pure (connections, output)
-    segment = sort <$> some (oneOf "abcdefg") <* try space
+    segment = S.fromList <$> some (oneOf "abcdefg") <* try space
 
--- | Given a list of signal patterns, find the representations of digits with
--- a unique number of segments. Return a map from digit to segments.
-findEasyNumbers :: [String] -> IntMap String
-findEasyNumbers = foldl go IM.empty
-  where
-    go found pat =
-      maybe found (\n -> IM.insert n pat found) $
-        IM.lookup (length pat) easy
+-- | A map from a set of ten signal patterns to a map from signal pattern to digit.
+problemSpace :: Map (Set (Set Char)) (Map (Set Char) Int)
+problemSpace =
+  M.fromList
+    [ ( S.fromList $
+          (map . S.map) (segmentTranslations M.!) $
+            IM.elems referenceDigits,
+        M.fromList
+          [ (S.map (segmentTranslations M.!) signalPattern, digit)
+            | (digit, signalPattern) <- IM.toList referenceDigits
+          ]
+      )
+      | permutation <- permutations "abcdefg",
+        let segmentTranslations = M.fromList (zip "abcdefg" permutation)
+    ]
 
--- | A map from number of segments used to the digit they represent.
--- All digits are uniqe in the number of segments required.
-easy :: IntMap Int
-easy = IM.foldlWithKey go IM.empty referenceDigits
-  where
-    go acc n pat =
-      if all ((pat ==) <||> ((/=) `on` length) pat) referenceDigits
-        then IM.insert (length pat) n acc
-        else acc
-
-deduceNumber :: Int -> Int -> Int -> [String] -> IntMap String -> IntMap String
-deduceNumber target source overlaps patterns found =
-  let len = length (referenceDigits IM.! target)
-   in maybe found (\pat -> IM.insert target pat found) $
-        find ((== overlaps) . countOverlaps found source) $
-          filter ((== len) . length) $
-            patterns \\ IM.elems found
-
-countOverlaps :: IntMap String -> Int -> String -> Int
-countOverlaps found known str = length $ isect str $ found IM.! known
-
--- | A map from digit to canonical (sorted) segment representation.
-referenceDigits :: IntMap String
+-- | A map from digit to canonical signal pattern.
+referenceDigits :: IntMap (Set Char)
 referenceDigits =
-  IM.fromList
+  IM.map S.fromList . IM.fromList $
     [ (0, "abcefg"),
       (1, "cf"),
       (2, "acdeg"),
