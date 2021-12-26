1b65bc31e91854d75d4d2e4f69371f776a2591a1
Eric Bailey <eric@ericb.me>
2021-12-25 23:33:45 -0600

    [2021.09] Add Haskell solution to Part Two

--- src/AdventOfCode/Year2021/Day09.hs
+++ src/AdventOfCode/Year2021/Day09.hs
@@ -5,20 +5,30 @@ import AdventOfCode.TH (defaultMain, inputFilePath)
 import Control.Lens (ifoldl')
 import Data.Char (digitToInt)
 import Data.Ix (Ix, inRange)
+import Data.List (sortOn)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.Set (Set)
+import Data.Ord (Down (Down))
+import Data.Set (Set, (\\))
 import qualified Data.Set as S
 import Linear (V2 (..))
-import Text.Trifecta
+import Text.Trifecta (Parser, digit, newline, sepEndBy, some)
+
+type Model = (HeightMap, (Point, Point))
+
+type HeightMap = Map (V2 Int) Height
+
+type Point = V2 Int
+
+type Height = Int
 
 main :: IO ()
 main = $(defaultMain)
 
-getInput :: IO (Map (V2 Int) Int)
+getInput :: IO Model
 getInput = parseInput model $(inputFilePath)
 
-example :: Map (V2 Int) Int
+example :: Model
 example =
   mkModel
     [ [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
@@ -28,28 +38,42 @@ example =
       [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
     ]
 
-partOne :: Map (V2 Int) Int -> Int
-partOne m = M.foldl' ((+) . succ) 0 (findLowPoints m)
+partOne :: Model -> Int
+partOne = sum . fmap succ . findLowPoints
 
-partTwo :: Map (V2 Int) Int -> Int
-partTwo = undefined
+partTwo :: Model -> Int
+partTwo modell =
+  product . take 3 . sortOn Down $
+    map (length . findBasin modell) $
+      M.keys (findLowPoints modell)
 
-model :: Parser (Map (V2 Int) Int)
+model :: Parser Model
 model = mkModel <$> some (digitToInt <$> digit) `sepEndBy` newline
 
-mkModel :: [[Int]] -> Map (V2 Int) Int
-mkModel = ifoldl' (ifoldl' . go) M.empty
+mkModel :: [[Height]] -> Model
+mkModel heightses = (kart, bounds)
   where
-    go y x acc n = M.insert (V2 x y) n acc
+    kart = ifoldl' (ifoldl' . go) M.empty heightses
+    go y x kartet height = M.insert (V2 x y) height kartet
+    bounds = (fst (M.findMin kart), fst (M.findMax kart))
 
-findLowPoints :: Map (V2 Int) Int -> Map (V2 Int) Int
-findLowPoints m = M.filterWithKey (isLowPoint m bounds) m
+findLowPoints :: Model -> HeightMap
+findLowPoints (kart, bounds) = M.filterWithKey (isLowPoint (kart, bounds)) kart
+
+isLowPoint :: Model -> Point -> Height -> Bool
+isLowPoint _ _ 9 = False
+isLowPoint (kart, bounds) point height =
+  all ((height <) . (kart M.!)) $
+    neighborsInRange bounds point
+
+findBasin :: Model -> Point -> Set Point
+findBasin (kart, bounds) lowPoint = S.foldl' go (S.singleton lowPoint) (S.singleton lowPoint)
   where
-    bounds = (fst (M.findMin m), fst (M.findMax m))
-
-isLowPoint :: Map (V2 Int) Int -> (V2 Int, V2 Int) -> V2 Int -> Int -> Bool
-isLowPoint _ _ _ 9 = False
-isLowPoint m bounds point n = all ((n <) . (m M.!)) (neighborsInRange bounds point)
+    go seen point =
+      let neighbors = S.filter ((< 9) . (kart M.!)) (neighborsInRange bounds point) \\ seen
+       in if S.null neighbors
+            then seen
+            else S.foldl' go (seen `S.union` neighbors) neighbors
 
 -- FIXME: generalize
 neighborsInRange :: (Ix a, Num a) => (V2 a, V2 a) -> V2 a -> Set (V2 a)
