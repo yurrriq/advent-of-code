2cb28a653360c94612a0a3ef77518bd7227860df
Eric Bailey <eric@ericb.me>
2022-12-07 22:28:58 -0600

    [2022.03] Add Haskell solution to Part Two

--- src/AdventOfCode/Year2022/Day03.hs
+++ src/AdventOfCode/Year2022/Day03.hs
@@ -5,29 +5,37 @@ import AdventOfCode.TH (defaultMain, inputFilePath)
 import Control.Applicative ((<|>))
 import Data.Char (ord)
 import Data.List (intersect, nub)
+import Data.List.Split (chunksOf)
 import Data.Monoid (Sum (..), getSum)
 import Text.Trifecta (Parser, lower, newline, sepEndBy, some, upper)
-
-data Item
-  = Lower Char
-  | Upper Char
-  deriving (Eq, Show)
 
 main :: IO ()
 main = $(defaultMain)
 
-partOne :: [([Item], [Item])] -> Int
-partOne =
-  getSum . mconcat
-    . foldMap
-      ( map (Sum . prioritize)
-          . uncurry intersect'
-      )
+partOne :: [[Int]] -> Int
+partOne = day03 (uncurry intersect . halve)
+  where
+    halve xs = splitAt (length xs `div` 2) xs
 
-partTwo :: [([Item], [Item])] -> Int
-partTwo = undefined
+partTwo :: [[Int]] -> Int
+partTwo = day03 (foldl1 intersect') . chunksOf 3
+  where
+    intersect' xs ys = nub (xs `intersect` ys)
 
-example :: IO [([Item], [Item])]
+day03 :: (a -> [Int]) -> [a] -> Int
+day03 f = getSum . mconcat . foldMap (map Sum . take 1 . f)
+
+getInput :: IO [[Int]]
+getInput = parseInput rucksacks $(inputFilePath)
+
+rucksacks :: Parser [[Int]]
+rucksacks = some prioritizedItem `sepEndBy` newline
+  where
+    prioritizedItem = lowerItem <|> upperItem
+    lowerItem = subtract 96 . ord <$> lower
+    upperItem = subtract 38 . ord <$> upper
+
+example :: IO [[Int]]
 example =
   parseString rucksacks . unlines $
     [ "vJrwpWtwJgWrhcsFMMfFFhFp",
@@ -37,22 +45,3 @@ example =
       "ttgJtRGJQctTZtZT",
       "CrZsJsPPZsGzwwsLwLmpwMDw"
     ]
-
-getInput :: IO [([Item], [Item])]
-getInput = parseInput rucksacks $(inputFilePath)
-
-rucksacks :: Parser [([Item], [Item])]
-rucksacks = rucksack `sepEndBy` newline
-  where
-    rucksack = halve <$> some item
-    item = (Lower <$> lower) <|> (Upper <$> upper)
-
-prioritize :: Item -> Int
-prioritize (Lower c) = ord c - 96
-prioritize (Upper c) = ord c - 38
-
-halve :: [a] -> ([a], [a])
-halve xs = splitAt (length xs `div` 2) xs
-
-intersect' :: Eq a => [a] -> [a] -> [a]
-intersect' xs ys = nub (xs `intersect` ys)
