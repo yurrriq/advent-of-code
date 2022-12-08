990b02dfb6f082ee503fcecf189d3c806696cba6
Eric Bailey <eric@ericb.me>
2022-12-07 23:18:32 -0600

    [2022.04] Add Haskell solution to Part One

---
+++ src/AdventOfCode/Year2022/Day04.hs
@@ -0,0 +1,40 @@
+module AdventOfCode.Year2022.Day04 where
+
+import AdventOfCode.Input (parseInput, parseString)
+import AdventOfCode.TH (defaultMain, inputFilePath)
+import Data.Ix (Ix, inRange)
+import Text.Trifecta (Parser, char, comma, decimal, newline, sepEndBy)
+
+main :: IO ()
+main = $(defaultMain)
+
+partOne :: [((Integer, Integer), (Integer, Integer))] -> Int
+partOne = length . filter (\(ab, cd) -> ab `fullyContains` cd || cd `fullyContains` ab)
+
+partTwo :: [((Integer, Integer), (Integer, Integer))] -> Int
+partTwo = undefined
+
+getInput :: IO [((Integer, Integer), (Integer, Integer))]
+getInput = parseInput assignmentPairs $(inputFilePath)
+
+assignmentPairs :: Parser [((Integer, Integer), (Integer, Integer))]
+assignmentPairs = assignmentPair `sepEndBy` newline
+  where
+    assignmentPair = (,) <$> (assignment <* comma) <*> assignment
+    assignment = (,) <$> (decimal <* char '-') <*> decimal
+
+fullyContains :: Ix a => (a, a) -> (a, a) -> Bool
+fullyContains (a, b) (c, d) = inRange (a, b) c && inRange (a, b) d
+
+infix 4 `fullyContains`
+
+example :: IO [((Integer, Integer), (Integer, Integer))]
+example =
+  parseString assignmentPairs . unlines $
+    [ "2-4,6-8",
+      "2-3,4-5",
+      "5-7,7-9",
+      "2-8,3-7",
+      "6-6,4-6",
+      "2-6,4-8"
+    ]
