d2aab220938add3cff20b1c810495e41a221acc1
Eric Bailey <eric@ericb.me>
2022-12-12 19:54:40 -0600

    [2022.06] Add Haskell solution to Part Two

--- src/AdventOfCode/Year2022/Day06.hs
+++ src/AdventOfCode/Year2022/Day06.hs
@@ -1,50 +1,42 @@
-{-# LANGUAGE DataKinds #-}
-
 module AdventOfCode.Year2022.Day06 where
 
 import AdventOfCode.Input (parseInput, parseString)
-import AdventOfCode.TH (defaultMain, inputFilePath)
+import AdventOfCode.TH (inputFilePath)
 import Control.Applicative (liftA2)
 import Data.Char (isAlpha)
-import Data.Vector.Sized (Vector)
-import qualified Data.Vector.Sized as VS
-import Text.Trifecta (Parser, letter, manyTill, satisfy, try)
+import Text.Trifecta (Parser, letter, manyTill, satisfy, some, try)
 
 main :: IO ()
-main = $(defaultMain)
-
-partOne :: Int -> Int
-partOne = id
-
-partTwo :: Int -> Int
-partTwo = undefined
-
-getInput :: IO Int
-getInput = parseInput datastream $(inputFilePath)
-
-datastream :: Parser Int
-datastream = (4 +) . length <$> manyTill letter (try sop)
-
-sop :: Parser (Vector 4 Char)
-sop =
+main =
   do
-    a <- letter
-    b <- satisfy (isAlpha <&&> (/= a))
-    c <- satisfy (isAlpha <&&> (`notElem` [a, b]))
-    d <- satisfy (isAlpha <&&> (`notElem` [a, b, c]))
-    case VS.fromList [a, b, c, d] of
-      Just xs -> pure xs
-      Nothing -> error "Can't make vector"
+    input <- getInput
+    putStr "Part One: "
+    n <- partOne input
+    print n
+    putStr "Part Two: "
+    m <- (n +) <$> partTwo (drop n input)
+    print m
 
-examples :: IO [Int]
-examples =
-  mapM
-    (parseString datastream)
-    [ "bvwbjplbgvbhsrlpgdmjqwftvncz",
-      "nppdvjthqldpwncqszvftbrmjlhg",
-      "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
-      "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
-    ]
+partOne :: String -> IO Int
+partOne = parseString (findSop 4)
+
+partTwo :: String -> IO Int
+partTwo = parseString (findSop 14)
+
+getInput :: IO String
+getInput = parseInput (some letter) $(inputFilePath)
+
+findSop :: Int -> Parser Int
+findSop n = (n +) . length <$> manyTill letter (try (sop n))
+
+sop :: Int -> Parser [Char]
+sop = go []
+  where
+    go seen 0 = pure (reverse seen)
+    go seen n =
+      do
+        c <- satisfy (isAlpha <&&> (`notElem` seen))
+        go (c : seen) (n - 1)
 
 (<&&>) :: Applicative f => f Bool -> f Bool -> f Bool
 (<&&>) = liftA2 (&&)
