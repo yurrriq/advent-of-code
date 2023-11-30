d98d3b92a7bd6b87efd46e95c663aa4e8df2f5f5
Eric Bailey <eric@ericb.me>
2023-11-29 23:35:21 -0600

    chore: start pulling out flake modules

---
+++ nix/haskell.nix
@@ -0,0 +1,57 @@
+{ ... }:
+
+{
+  flake = {
+    overlays.haskell = _final: prev: {
+      haskellPackages = prev.haskellPackages.override {
+        overrides = _hfinal: hprev: {
+          digits = hprev.callCabal2nix "digits"
+            (prev.fetchFromGitHub {
+              owner = "yurrriq";
+              repo = "digits";
+              rev = "c3a2c2bacc4a2e2c51beefa2fdb90da9a5bddf6b";
+              hash = "sha256-/n2gf33zShj6LexHRplp975teCZyLAsg0rmXK9AHoK0=";
+            })
+            { };
+        };
+      };
+    };
+  };
+
+  perSystem = { pkgs, self', ... }: {
+    devShells.haskell = pkgs.mkShell {
+      inputsFrom = [
+        self'.packages.advent-of-code.env
+      ];
+
+      nativeBuildInputs = with pkgs; [
+        cabal-install
+        ghc
+        ghcid
+        haskell-language-server
+        haskellPackages.pointfree
+      ];
+    };
+
+    packages = {
+      advent-of-code = pkgs.haskellPackages.callCabal2nix
+        "advent-of-code"
+        (pkgs.nix-gitignore.gitignoreSource [ ] ../.)
+        { };
+
+      default = self'.packages.advent-of-code;
+    };
+
+    treefmt = {
+      programs = {
+        hlint.enable = true;
+        ormolu = {
+          enable = true;
+          ghcOpts = [
+            "TemplateHaskell"
+          ];
+        };
+      };
+    };
+  };
+}
---
+++ nix/noweb.nix
@@ -0,0 +1,61 @@
+{ ... }:
+
+{
+  flake = {
+    overlays.noweb = final: prev: {
+      noweb = prev.noweb.override {
+        icon-lang = prev.icon-lang.override {
+          withGraphics = false;
+        };
+      };
+
+      xelatex-noweb = prev.texlive.combine {
+        inherit (prev.texlive) scheme-small;
+        inherit (final) noweb;
+        # Packages for Noweb
+        inherit (prev.texlive)
+          braket
+          catchfile
+          datatool
+          datetime
+          # dirtytalk
+          fmtcount
+          framed
+          fvextra
+          # glossaries
+          # glossaries-extra
+          hardwrap
+          ifplatform
+          latexmk
+          mfirstuc
+          minted
+          substr
+          titlesec
+          # tkz-base
+          todonotes
+          tufte-latex
+          xetex
+          xindy
+          xfor
+          xstring
+          ;
+        # Packages for GAP
+        inherit (prev.texlive)
+          helvetic
+          enumitem
+          ;
+      };
+    };
+  };
+
+  perSystem = { pkgs, ... }: {
+    devShells.noweb = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        noweb
+        python3Packages.pygments
+        rnix-lsp
+        xelatex-noweb
+      ];
+    };
+  };
+}
