c4d55e9d6cf6322be37d944460f6d289d980951e
Eric Bailey <eric@ericb.me>
2023-12-08 00:39:48 -0600

    [2023.03] Haskell: ignore periods

--- src/AdventOfCode/Year2023/Day03.hs
+++ src/AdventOfCode/Year2023/Day03.hs
@@ -10,6 +10,7 @@ import Data.FastDigits (undigits)
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import Data.List (intercalate)
+import Data.Set (Set)
 import qualified Data.Set as Set
 import Linear (V2 (..))
 import Text.Trifecta hiding (parseString)
@@ -38,33 +39,29 @@ instance Ord Datum where
 
 newtype Schematic = Schematic {unSchematic :: HashMap (V2 Int) Datum}
 
-instance Show Schematic where
-  show (Schematic hm) =
-    intercalate "\n" $
-      [ concat
-          [ maybe "" show (HM.lookup (V2 x y) hm)
-            | x <- [0 .. width]
-          ]
-        | y <- [0 .. height]
-      ]
-    where
-      V2 width height = maximum (HM.keys hm)
-
 main :: IO ()
 main = $(defaultMain)
 
 partOne :: Schematic -> Int
 partOne (Schematic hm) = let Number n = mconcat ds in n
   where
-    ds = HM.elems (HM.filterWithKey adjacentToSymbol hm)
-    adjacentToSymbol (V2 x y) (Number n) =
-      any isSymbol $
-        foldr ((<>) . neighborsOf) Set.empty $
-          flip V2 y <$> [x .. x + length (show n) - 1]
-    adjacentToSymbol _ _ = False
+    ds = HM.elems (HM.filterWithKey (isNumberAdjacentToSymbol hm) hm)
+
+isNumberAdjacentToSymbol :: HashMap (V2 Int) Datum -> V2 Int -> Datum -> Bool
+isNumberAdjacentToSymbol hm (V2 x y) (Number n) =
+  any isSymbol $ allNeighborsOf (V2 x y) n
+  where
     isSymbol k = case HM.lookup k hm of
       Just (Symbol _) -> True
       _ -> False
+isNumberAdjacentToSymbol _ _ _ = False
+
+allNeighborsOf :: Show a => V2 Int -> a -> Set (V2 Int)
+allNeighborsOf (V2 x y) n =
+  flip Set.difference (Set.fromList points) $
+    foldr ((<>) . neighborsOf) Set.empty points
+  where
+    points = flip V2 y <$> [x .. x + length (show n) - 1]
 
 partOneExample :: IO Int
 partOneExample = partOne <$> parseString schematic example
@@ -83,10 +80,15 @@ mkSchematic xxs = Schematic hm
   where
     (hm, _) = ifoldl' (ifoldl' . go) (HM.empty, []) xxs
     go _ _ (hm', ns) (Number n) = (hm', n : ns)
-    go y x (hm', []) d = (HM.insert (V2 x y) d hm', [])
-    go y x (hm', ns) d =
+    go y x (hm', []) d@(Symbol _) = (HM.insert (V2 x y) d hm', [])
+    go _ _ (hm', []) Period = (hm', [])
+    go y x (hm', ns) d@(Symbol _) =
       ( HM.insert (V2 (x - length ns) y) (Number (undigits' ns)) $
           HM.insert (V2 x y) d hm',
+        []
+      )
+    go y x (hm', ns) Period =
+      ( HM.insert (V2 (x - length ns) y) (Number (undigits' ns)) hm',
         []
       )
     undigits' = fromInteger . undigits (10 :: Int)
