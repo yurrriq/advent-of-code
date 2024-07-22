8c058d797d62d2f1849a48978f667aaebf20e401
Eric Bailey <eric@ericb.me>
2024-07-22 12:28:24 -0500

    build(deps): nix flake update
    Flake lock file updates:
    
    • Updated input 'emacs-overlay':
        'github:nix-community/emacs-overlay/c806a21d556a5d2c623ddfa24ca35e4138df62e8' (2024-05-13)
      → 'github:nix-community/emacs-overlay/7164cf7ca4318ffad776682ef8cf8aa9e24a1216' (2024-07-22)
    • Updated input 'emacs-overlay/nixpkgs-stable':
        'github:NixOS/nixpkgs/44072e24566c5bcc0b7aa9178a0104f4cfffab19' (2024-05-12)
      → follows 'nixpkgs-stable'
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/e5d10a24b66c3ea8f150e47dfdb0416ab7c3390e' (2024-05-02)
      → 'github:hercules-ci/flake-parts/9227223f6d922fee3c7b190b2cc238a99527bbb7' (2024-07-03)
    • Updated input 'flake-parts/nixpkgs-lib':
        'https://github.com/NixOS/nixpkgs/archive/50eb7ecf4cd0a5756d7275c8ba36790e5bd53e33.tar.gz?narHash=sha256-QBx10%2Bk6JWz6u7VsohfSw8g8hjdBZEf8CFzXH1/1Z94%3D' (2024-05-02)
      → 'https://github.com/NixOS/nixpkgs/archive/5daf0514482af3f97abaefc78a6606365c9108e2.tar.gz?narHash=sha256-Fm2rDDs86sHy0/1jxTOKB1118Q0O3Uc7EC0iXvXKpbI%3D' (2024-07-01)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/3281bec7174f679eabf584591e75979a258d8c40' (2024-05-12)
      → 'github:nixos/nixpkgs/c19d62ad2265b16e2199c5feb4650fe459ca1c46' (2024-07-21)
    • Updated input 'nixpkgs-stable':
        'github:nixos/nixpkgs/8a4282c38b6cbea9f0989c0eafc6ce1837a26442' (2024-05-13)
      → 'github:nixos/nixpkgs/b9de2b60d0486038b5a028196c4d821474054793' (2024-07-22)
    • Updated input 'pre-commit-hooks-nix':
        'github:cachix/pre-commit-hooks.nix/c182c876690380f8d3b9557c4609472ebfa1b141' (2024-05-13)
      → 'github:cachix/git-hooks.nix/f451c19376071a90d8c58ab1a953c6e9840527fd' (2024-07-15)
    • Removed input 'pre-commit-hooks-nix/flake-utils'
    • Removed input 'pre-commit-hooks-nix/flake-utils/systems'
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/c6aaf729f34a36c445618580a9f95a48f5e4e03f' (2024-04-25)
      → 'github:numtide/treefmt-nix/888bfb10a9b091d9ed2f5f8064de8d488f7b7c97' (2024-07-20)

--- flake.nix
+++ flake.nix
@@ -3,18 +3,21 @@
 
   inputs = {
     emacs-overlay = {
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs-stable";
+      };
       url = "github:nix-community/emacs-overlay";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-24.05";
     pre-commit-hooks-nix = {
       inputs = {
         nixpkgs.follows = "nixpkgs";
         nixpkgs-stable.follows = "nixpkgs-stable";
       };
-      url = "github:cachix/pre-commit-hooks.nix";
+      url = "github:cachix/git-hooks.nix";
     };
     treefmt-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
@@ -44,6 +47,11 @@
         lib.composeManyExtensions
           (lib.attrValues
             (lib.filterAttrs (name: _: name != "default") self.overlays));
+
+      # FIXME: v2 works differently, I guess.
+      flake.overlays.treefmt = _final: prev: {
+        treefmt = prev.treefmt1;
+      };
 
       perSystem = { config, pkgs, self', system, ... }: {
         _module.args.pkgs = import inputs.nixpkgs {
