d6981e80b11b8a155dd651c2303f4bbd7ce752a2
Eric Bailey <eric@ericb.me>
2024-12-03 15:06:26 -0600

    feat(2024.01-haskell): solve Part Two naively

--- src/AdventOfCode/Year2024/Day01.hs
+++ src/AdventOfCode/Year2024/Day01.hs
@@ -2,18 +2,28 @@ module AdventOfCode.Year2024.Day01 where
 
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
+import AdventOfCode.Util (frequencies)
 import Data.List (sort, transpose)
+import Data.List.Extra (sumOn')
+import qualified Data.Map as Map
 import Text.Trifecta (count, natural, some)
 
 main :: IO ()
 main = $(defaultMain)
 
-partOne :: (Num a, Ord a) => [[a]] -> a
-partOne [xs, ys] = sum $ zipWith ((abs .) . (-)) (sort xs) (sort ys)
-partOne _ = error "Ope!"
+partOne :: (Num a, Ord a) => ([a], [a]) -> a
+partOne (xs, ys) = sum $ zipWith ((abs .) . (-)) (sort xs) (sort ys)
 
-partTwo :: (Num a, Ord a) => [[a]] -> a
-partTwo = undefined
+partTwo :: (Num a, Ord a) => ([a], [a]) -> a
+partTwo (xs, ys) = sumOn' go xs
+  where
+    go x = x * fromIntegral (Map.findWithDefault 0 x freqs)
+    freqs = frequencies ys
 
-getInput :: IO [[Integer]]
-getInput = transpose <$> parseInput (some (count 2 natural)) $(inputFilePath)
+getInput :: IO ([Integer], [Integer])
+getInput =
+  do
+    [xs, ys] <-
+      transpose
+        <$> parseInput (some (count 2 natural)) $(inputFilePath)
+    pure (xs, ys)
