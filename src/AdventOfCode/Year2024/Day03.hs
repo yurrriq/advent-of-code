96b22812cdc87bfdca41ac6155be5812ade80005
Eric Bailey <eric@ericb.me>
2024-12-04 15:16:39 -0600

    feat(2024.03-haskell): solve Part Two

--- src/AdventOfCode/Year2024/Day03.hs
+++ src/AdventOfCode/Year2024/Day03.hs
@@ -4,6 +4,7 @@ import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
 import Control.Applicative ((<|>))
 import Control.Monad (void)
+import Data.Either (rights)
 import Data.List.Extra (sumOn')
 import Text.Parser.LookAhead (lookAhead)
 import Text.Trifecta
@@ -21,21 +22,39 @@ import Text.Trifecta
 main :: IO ()
 main = $(defaultMain)
 
-partOne :: [(Integer, Integer)] -> Integer
-partOne = sumOn' (uncurry (*))
+partOne :: [Either Bool (Integer, Integer)] -> Integer
+partOne = sumOn' (uncurry (*)) . rights
 
-partTwo :: [(Integer, Integer)] -> Integer
-partTwo = undefined
-
-getInput :: IO [(Integer, Integer)]
-getInput = parseInput (some (skipNoise *> multiplication)) $(inputFilePath)
+partTwo :: [Either Bool (Integer, Integer)] -> Integer
+partTwo = go
   where
-    multiplication =
-      try $ string "mul" *> parens ((,) <$> (decimal <* comma) <*> decimal)
+    go [] = 0
+    go (Left False : muls) = go (dropWhile (/= Left True) muls)
+    go muls =
+      let (xs, ys) = span (/= Left False) muls
+       in partOne xs + go ys
+
+getInput :: IO [Either Bool (Integer, Integer)]
+getInput = parseInput (some go) $(inputFilePath)
+  where
+    go =
+      skipNoise
+        *> ( (Left <$> toggle)
+               <|> (Right <$> multiplication)
+           )
     skipNoise =
       do
         void . manyTill anyChar $
           lookAhead
             ( void multiplication
+                <|> void toggle
                 <|> eof
             )
+    toggle =
+      try $
+        (True <$ string "do()")
+          <|> (False <$ string "don't()")
+    multiplication =
+      try $
+        string "mul"
+          *> parens ((,) <$> (decimal <* comma) <*> decimal)
