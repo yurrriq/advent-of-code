83a863e8240d6ae9f08bab0b0248f551604a56f6
Eric Bailey <eric@ericb.me>
2024-12-03 20:22:40 -0600

    feat(2024.02-haskell): solve Part Two naively

--- src/AdventOfCode/Year2024/Day02.hs
+++ src/AdventOfCode/Year2024/Day02.hs
@@ -2,7 +2,7 @@ module AdventOfCode.Year2024.Day02 where
 
 import AdventOfCode.Input (parseInput)
 import AdventOfCode.TH (defaultMain, inputFilePath)
-import AdventOfCode.Util (count)
+import AdventOfCode.Util (count, holes)
 import Data.Ix (inRange)
 import Text.Trifecta (char, decimal, newline, sepBy1, sepEndBy)
 
@@ -11,14 +11,9 @@ main = $(defaultMain)
 
 partOne :: [[Integer]] -> Int
 partOne = count isSafe
-  where
-    isSafe xs = all (== head signs) (tail signs) && all (inRange (1, 3) . abs) diffs
-      where
-        diffs = zipWith (-) (tail xs) xs
-        signs = map signum diffs
 
 partTwo :: [[Integer]] -> Int
-partTwo = undefined
+partTwo = count (any isSafe) . map (map snd . holes)
 
 getInput :: IO [[Integer]]
 getInput = parseInput ((decimal `sepBy1` char ' ') `sepEndBy` newline) $(inputFilePath)
@@ -32,3 +27,9 @@ example =
     [8, 6, 4, 4, 1],
     [1, 3, 6, 7, 9]
   ]
+
+isSafe :: [Integer] -> Bool
+isSafe xs = all (== head signs) (tail signs) && all (inRange (1, 3) . abs) diffs
+  where
+    diffs = zipWith (-) (tail xs) xs
+    signs = map signum diffs
