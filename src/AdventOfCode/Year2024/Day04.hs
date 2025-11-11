76d2d2847d37169f3f8658aedc12ab2aeca7e754
Eric Bailey <eric@ericb.me>
2025-11-10 22:47:31 -0600

    refactor(2024.04-haskell): adopt advent-of-code-api

--- src/AdventOfCode/Year2024/Day04.hs
+++ src/AdventOfCode/Year2024/Day04.hs
@@ -1,52 +1,71 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module AdventOfCode.Year2024.Day04 where
 
-import AdventOfCode.Input (parseInput, parseString)
-import AdventOfCode.TH (defaultMain, inputFilePath)
+import AdventOfCode.Input (parseInputAoC, parseString)
+import AdventOfCode.Puzzle (Puzzle, runPuzzle)
+import AdventOfCode.TH (evalPuzzle)
 import AdventOfCode.Util (count)
-import Data.List (tails, transpose)
+import Control.Lens (makeLenses, (<.=))
 import Data.List.Extra (dropEnd, sumOn')
 import Data.Universe.Helpers (diagonals)
 import Linear (V2 (..))
-import Text.Trifecta (Parser, manyTill, newline, oneOf, some)
+import Relude
+import Text.Trifecta (Parser, manyTill, newline, oneOf)
+
+data PuzzleState
+  = PuzzleState
+  { _answerOne :: !Int,
+    _answerTwo :: !Int
+  }
+  deriving (Eq, Generic, Show)
+
+makeLenses ''PuzzleState
+
+emptyPuzzleState :: PuzzleState
+emptyPuzzleState = PuzzleState 0 0
 
 main :: IO ()
-main = $(defaultMain)
+main = $(evalPuzzle)
 
-partOne :: [[Char]] -> Int
-partOne grid =
-  sum
-    [ countHorizontal grid,
-      countHorizontal gridRotatedRight90,
-      countDiagonal grid,
-      countDiagonal gridRotatedRight90
-    ]
+partOne :: Puzzle [[Char]] PuzzleState Int
+partOne = do
+  grid <- ask
+  let gridRotatedRight90 = transpose (reverse grid)
+  answerOne
+    <.= sum
+      [ countHorizontal grid,
+        countHorizontal gridRotatedRight90,
+        countDiagonal grid,
+        countDiagonal gridRotatedRight90
+      ]
   where
     countHorizontal =
       sumOn' (count ((`elem` ["XMAS", "SAMX"]) . take 4) . dropEnd 4 . tails)
     countDiagonal =
       countHorizontal . dropEnd 3 . drop 3 . diagonals
-    gridRotatedRight90 = transpose (reverse grid)
 
-partTwo :: [[Char]] -> Int
-partTwo grid =
-  count
-    ((`elem` ["MMSS", "MSMS", "SMSM", "SSMM"]) . map (square grid) . corners)
-    [ V2 x y
-    | x <- [0 .. size - 3],
-      y <- [0 .. size - 3],
-      square grid (V2 x y + 1) == 'A'
-    ]
-  where
-    size = length grid
+partTwo :: Puzzle [[Char]] PuzzleState Int
+partTwo = do
+  grid <- ask
+  let size = length grid
+  answerTwo
+    <.= count
+      ((`elem` ["MMSS", "MSMS", "SMSM", "SSMM"]) . mapMaybe (square grid) . corners)
+      [ V2 x y
+      | x <- [0 .. size - 3],
+        y <- [0 .. size - 3],
+        square grid (V2 x y + 1) == Just 'A'
+      ]
 
 corners :: (Num a) => V2 a -> [V2 a]
 corners xy = [xy + V2 a b | a <- [0, 2], b <- [0, 2]]
 
-square :: [[Char]] -> V2 Int -> Char
-square grid (V2 x y) = grid !! y !! x
+square :: [[Char]] -> V2 Int -> Maybe Char
+square grid (V2 x y) = grid !!? y >>= maybeAt x
 
 getInput :: IO [[Char]]
-getInput = parseInput wordSearch $(inputFilePath)
+getInput = parseInputAoC 2024 4 wordSearch
 
 wordSearch :: Parser [[Char]]
 wordSearch = some (manyTill (oneOf "XMAS") newline)
