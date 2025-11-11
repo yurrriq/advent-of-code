70995d73fe334a6a25d91568c3e6a1568ada53e2
Eric Bailey <eric@ericb.me>
2025-11-11 13:29:20 -0600

    refactor(2024.05-haskell): adopt advent-of-code-api

--- src/AdventOfCode/Year2024/Day05.hs
+++ src/AdventOfCode/Year2024/Day05.hs
@@ -1,31 +1,50 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module AdventOfCode.Year2024.Day05 where
 
-import AdventOfCode.Input (parseInput, parseString)
-import AdventOfCode.TH (defaultMain, inputFilePath)
-import Control.Arrow ((&&&))
-import Control.Monad (void)
+import AdventOfCode.Input (parseInputAoC, parseString)
+import AdventOfCode.Puzzle (Puzzle, runPuzzle)
+import AdventOfCode.TH (evalPuzzle)
+import Control.Lens (makeLenses, (<.=))
 import Data.Graph qualified as Graph
+import Data.Semigroup (Max (..), Min (..))
+import Relude
 import Text.Trifecta hiding (parseString)
 
+data PuzzleState
+  = PuzzleState
+  { _answerOne :: !Int,
+    _answerTwo :: !Int
+  }
+  deriving (Eq, Generic, Show)
+
+makeLenses ''PuzzleState
+
+emptyPuzzleState :: PuzzleState
+emptyPuzzleState = PuzzleState 0 0
+
 main :: IO ()
-main = $(defaultMain)
+main = $(evalPuzzle)
 
-partOne :: ([(Int, Int)], [[Int]]) -> Int
-partOne (rules, updates) =
-  sum
-    [ middle update
-      | update <- updates,
-        update == applyRules rules update
-    ]
+partOne :: Puzzle ([(Int, Int)], [[Int]]) PuzzleState Int
+partOne = do
+  (rules, updates) <- ask
+  (answerOne <.=)
+    $ sum
+    $ mapMaybe middle
+    $ filter (uncurry (==) . (id &&& applyRules rules)) updates
 
-partTwo :: ([(Int, Int)], [[Int]]) -> Int
-partTwo (rules, updates) =
-  sum
-    [ middle sorted
+partTwo :: Puzzle ([(Int, Int)], [[Int]]) PuzzleState Int
+partTwo = do
+  (rules, updates) <- ask
+  (answerTwo <.=)
+    $ sum
+    $ mapMaybe middle
+    $ [ sorted
       | update <- updates,
         let sorted = applyRules rules update,
         update /= sorted
-    ]
+      ]
 
 -- TODO: look into fgl instead
 applyRules :: [(Int, Int)] -> [Int] -> [Int]
@@ -34,11 +53,11 @@ applyRules rules update = filter (`elem` update) (Graph.topSort graph)
     graph = Graph.buildG bounds relevantRules
     relevantRules = filter (all (`elem` update)) rules
     bounds =
-      minimum &&& maximum $
-        foldMap (\(before, after) -> [before, after]) relevantRules
+      bimap getMin getMax
+        $ foldMap (\(before, after) -> (Min (min before after), Max (max before after))) relevantRules
 
 getInput :: IO ([(Int, Int)], [[Int]])
-getInput = parseInput safetyManual $(inputFilePath)
+getInput = parseInputAoC 2024 5 safetyManual
 
 safetyManual :: Parser ([(Int, Int)], [[Int]])
 safetyManual =
@@ -89,5 +108,5 @@ example =
   \61,13,29\n\
   \97,13,75,29,47\n"
 
-middle :: [a] -> a
-middle xs = xs !! (length xs `div` 2)
+middle :: [a] -> Maybe a
+middle xs = xs !!? (length xs `div` 2)
