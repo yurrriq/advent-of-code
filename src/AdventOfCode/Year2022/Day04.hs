3ee4a46c75902ab0059019588e0ba77847ac94ec
Eric Bailey <eric@ericb.me>
2025-11-13 22:55:48 -0600

    refactor(2022.04-haskell): adopt advent-of-code-api

--- src/AdventOfCode/Year2022/Day04.hs
+++ src/AdventOfCode/Year2022/Day04.hs
@@ -1,29 +1,32 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module AdventOfCode.Year2022.Day04 where
 
-import AdventOfCode.Input (parseInput, parseString)
-import AdventOfCode.TH (defaultMain, inputFilePath)
+import AdventOfCode.Input (parseInputAoC, parseString)
+import AdventOfCode.SimplePuzzle
+import AdventOfCode.TH (evalPuzzle)
 import Data.Ix (inRange)
-import Data.Tuple (swap)
+import Relude
 import Text.Trifecta (Parser, char, comma, decimal, newline, sepEndBy)
 
 main :: IO ()
-main = $(defaultMain)
+main = $(evalPuzzle)
 
-partOne :: [((Integer, Integer), (Integer, Integer))] -> Int
-partOne = day03 fullyContains
+partOne :: SimplePuzzle [((Integer, Integer), (Integer, Integer))] Int
+partOne = asks (solveWith fullyContains)
   where
     fullyContains (ab, (c, d)) = inRange ab c && inRange ab d
 
-partTwo :: [((Integer, Integer), (Integer, Integer))] -> Int
-partTwo = day03 overlaps
+partTwo :: SimplePuzzle [((Integer, Integer), (Integer, Integer))] Int
+partTwo = asks (solveWith overlaps)
   where
     overlaps (ab, (c, d)) = inRange ab c || inRange ab d
 
-day03 :: ((a, a) -> Bool) -> [(a, a)] -> Int
-day03 f = length . filter (liftA2 (||) f (f . swap))
+solveWith :: ((a, a) -> Bool) -> [(a, a)] -> Int
+solveWith f = length . filter (liftA2 (||) f (f . swap))
 
 getInput :: IO [((Integer, Integer), (Integer, Integer))]
-getInput = parseInput assignmentPairs $(inputFilePath)
+getInput = parseInputAoC 2022 4 assignmentPairs
 
 assignmentPairs :: Parser [((Integer, Integer), (Integer, Integer))]
 assignmentPairs = assignmentPair `sepEndBy` newline
@@ -31,13 +34,14 @@ assignmentPairs = assignmentPair `sepEndBy` newline
     assignmentPair = (,) <$> (assignment <* comma) <*> assignment
     assignment = (,) <$> (decimal <* char '-') <*> decimal
 
-example :: IO [((Integer, Integer), (Integer, Integer))]
+getExample :: IO [((Integer, Integer), (Integer, Integer))]
+getExample = parseString assignmentPairs example
+
+example :: String
 example =
-  parseString assignmentPairs . unlines $
-    [ "2-4,6-8",
-      "2-3,4-5",
-      "5-7,7-9",
-      "2-8,3-7",
-      "6-6,4-6",
-      "2-6,4-8"
-    ]
+  "2-4,6-8\n\
+  \2-3,4-5\n\
+  \5-7,7-9\n\
+  \2-8,3-7\n\
+  \6-6,4-6\n\
+  \2-6,4-8\n"
