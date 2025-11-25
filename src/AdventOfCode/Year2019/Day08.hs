fc13dcd614a5d22265a3dae972aca265bbc8aa11
Eric Bailey <eric@ericb.me>
2025-11-25 00:15:31 -0600

    refactor(2019.08-haskell): adopt advent-of-code-api

--- src/AdventOfCode/Year2019/Day08.hs
+++ src/AdventOfCode/Year2019/Day08.hs
@@ -1,17 +1,29 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module AdventOfCode.Year2019.Day08 where
 
-import AdventOfCode.Input (parseInput)
-import AdventOfCode.TH (defaultMain, inputFilePath)
-import Control.Applicative ((<|>))
-import Data.Function (on)
-import Data.List (minimumBy)
-import Text.Trifecta (Parser, char, count, some, (<?>))
+import AdventOfCode.Input (parseInputAoC)
+import AdventOfCode.Puzzle
+import AdventOfCode.Util qualified as Util
+import Data.List.Extra (minimumBy, sumOn')
+import Data.Text.IO qualified as TextIO
+import Relude
+import Text.Show qualified
+import Text.Trifecta (Parser, char, choice, count, (<?>))
 
 data Pixel
   = Black
   | White
   | Transparent
-  deriving (Enum, Eq)
+  deriving (Eq, Generic)
+
+instance Semigroup Pixel where
+  Transparent <> below = below
+  above <> _ = above
+
+instance Monoid Pixel where
+  mempty = Transparent
 
 instance Show Pixel where
   show Black = " "
@@ -25,42 +37,44 @@ type Layer = [Row]
 type Row = [Pixel]
 
 main :: IO ()
-main = $(defaultMain)
+main =
+  getInput >>= evaluatingPuzzle do
+    putStr "Part One: "
+    print =<< partOne
+    putStrLn "Part Two:"
+    liftIO . TextIO.putStrLn =<< partTwo
 
 getInput :: IO Image
-getInput = parseInput (image 25 6) $(inputFilePath)
+getInput = parseInputAoC 2019 8 (image 25 6)
 
-partOne :: Image -> Int
-partOne layers = numberOf White layer * numberOf Transparent layer
+partOne :: SimplePuzzle Image Int
+partOne =
+  asks
+    $ minimumBy (compare `on` numberOf Black)
+    >>> (numberOf White &&& numberOf Transparent)
+    >>> uncurry (*)
   where
-    layer = minimumBy (compare `on` numberOf Black) layers
-    numberOf :: (Eq a) => a -> [[a]] -> Int
-    numberOf x = sum . fmap (length . filter (== x))
+    numberOf x = sumOn' (Util.count (== x))
 
-partTwo :: Image -> String
-partTwo layers =
-  unlines . map (concatMap show) $
-    foldl decodeLayer (transparentLayer 25 6) layers
+partTwo :: SimplePuzzle Image Text
+partTwo =
+  asks
+    $ foldl' decodeLayer (transparentLayer 25 6)
+    >>> map (foldMap show)
+    >>> unlines
   where
-    decodeLayer :: Layer -> Layer -> Layer
-    decodeLayer = zipWith (zipWith decodePixel)
-    decodePixel :: Pixel -> Pixel -> Pixel
-    decodePixel Transparent below = below
-    decodePixel above _ = above
+    decodeLayer = zipWith (zipWith (<>))
 
 image :: Int -> Int -> Parser Image
-image width height = some layer
-  where
-    layer :: Parser Layer
-    layer = count height row
-    row :: Parser Row
-    row = count width pixel
+image width height = some (count height (count width pixel))
 
 pixel :: Parser Pixel
 pixel =
-  (char '0' *> pure Black <?> "A black pixel")
-    <|> (char '1' *> pure White <?> "A white pixel")
-    <|> (char '2' *> pure Transparent <?> "A transparent pixel")
+  choice
+    [ char '0' *> pure Black <?> "A black pixel",
+      char '1' *> pure White <?> "A white pixel",
+      char '2' *> pure Transparent <?> "A transparent pixel"
+    ]
 
 transparentLayer :: Int -> Int -> Layer
 transparentLayer width height = replicate height (replicate width Transparent)
