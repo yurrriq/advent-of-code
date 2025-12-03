1ca0039f0f52e40d686df2273d9c115dba9447ea
Eric Bailey <eric@ericb.me>
2025-12-03 05:43:22 -0600

    feat(2025-03/haskell): solve Part Two for the example

--- src/AdventOfCode/Year2025/Day03.hs
+++ src/AdventOfCode/Year2025/Day03.hs
@@ -8,11 +8,15 @@ import AdventOfCode.TH (defaultMainPuzzle)
 import AdventOfCode.Util (maybeFail, (<.>))
 import Data.Char (digitToInt)
 import Data.Foldable (maximum)
+import Data.Semigroup (Max (..))
 import Relude
 import Text.Trifecta (digit, newline, sepEndBy)
 
 getExample :: IO [[Int]]
-getExample = parseString (some (digitToInt <$> digit) `sepEndBy` newline) example
+getExample =
+  flip parseString example
+    $ some (digitToInt <$> digit)
+    `sepEndBy` newline
 
 example :: String
 example =
@@ -35,8 +39,23 @@ getInput =
     $ some (digitToInt <$> digit)
     `sepEndBy` newline
 
-partTwo :: SimplePuzzle [[Int]] ()
-partTwo = fail "not yet implemented"
+largestNumber :: Int -> [Int] -> Maybe Int
+largestNumber k bank
+  | k <= 0 = Nothing
+  | otherwise = getMax <$> go k bank
+  where
+    go 1 xs = Just (Max (maximum xs))
+    go n xs =
+      case uncons xs of
+        Nothing -> Nothing
+        Just (_, []) -> Nothing
+        Just (d, ds) ->
+          let candidate = (Max d * 10 ^ (n - 1) +) <$> go (n - 1) ds
+              nextCandidate = go n ds
+           in candidate <> nextCandidate
+
+partTwo :: SimplePuzzle [[Int]] Int
+partTwo = ask >>= maybeFail "ope!" . sum <.> traverse (largestNumber 12)
 
 main :: IO ()
 main = $(defaultMainPuzzle)
